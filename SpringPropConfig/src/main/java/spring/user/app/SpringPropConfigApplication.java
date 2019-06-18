329c4d7f070cba6e751dd4052ae64009131db752
devadineshkumar <devadineshkumar@gmail.com>
2019-06-18 07:40:47 +0530

    spring tutorial to add

---
+++ SpringPropConfig/src/main/java/spring/user/app/SpringPropConfigApplication.java
@@ -0,0 +1,13 @@
+package spring.user.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPropConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPropConfigApplication.class, args);
+	}
+
+}
---
+++ SpringPropConfig/src/test/java/spring/user/app/SpringPropConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package spring.user.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringPropConfigApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
